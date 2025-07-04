a6847d3f72ba2856269e773e9c31c978c478a6d4
sivay1 <newone2e@gmail.com>
2025-07-04 19:03:31 +0530

    cache feature

--- async/apicalls.js
+++ async/apicalls.js
@@ -1,5 +1,16 @@
 const callApi = async () => {
     try{
+        const cachedUsers = localStorage.getItem("cachedUsers");
+        const cachedPosts = localStorage.getItem("cachedPosts");
+        let userData;
+        let postData;
+        if (cachedUsers && cachedPosts) {
+            // Step 2: Use cached data if available
+            console.log("Using cached data");
+            userData = JSON.parse(cachedUsers);
+            postData = JSON.parse(cachedPosts);
+        }
+        
         const userResponse = await fetch('https://jsonplaceholder.typicode.com/users'
         //     ,{
         //     method: "GET",
@@ -11,8 +22,12 @@ const callApi = async () => {
         const postsResponse = await fetch('https://jsonplaceholder.typicode.com/posts')
 
         const [userRes, postRes] = await Promise.all([userResponse, postsResponse]);
-        const userData = await userRes.json();
-        const postData = await postRes.json();
+        userData = await userRes.json();
+        postData = await postRes.json();
+        
+        localStorage.setItem("cachedUsers", JSON.stringify(userData));
+        localStorage.setItem("cachedPosts", JSON.stringify(postData));
+        
         // const data = await userResponse.json();
         console.log(userData[0].name); //output is array of objects
         console.log(postData[0].title);
